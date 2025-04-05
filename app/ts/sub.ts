636d83425e883f886c690d4f327dc6ca06eab281
Joshua Thome <joshpthome@gmail.com>
2025-04-05 12:56:49 -0500

    Drilling first pass

--- app/ts/sub.ts
+++ app/ts/sub.ts
@@ -3,6 +3,7 @@ import { camera } from "./camera.js"
 import { Entity } from "./entity.js"
 import { ACTIONS } from "./input.js"
 import { moveAngleTowards } from "./math.js"
+import { tileMap } from "./tilemap.js"
 
 const THRUST_SPEED = 300
 const RESURFACE_SPEED_BONUS = 150
@@ -11,6 +12,9 @@ const OXYGEN_DRAIN_RATE = 1 / 60 // 1 minute
 const OXYGEN_REFILL_RATE = 1 / 5 // 5 seconds
 const HURT_INVULN_TIME = 1.0
 const TURN_SPEED = Math.PI // radians per second
+const DRILL_FUEL_COST = 0.025 // cost of breaking one tile
+const REFUEL_MAX_DIST = 64
+const REFUEL_RATE = 0.1 // 10 seconds to refuel
 
 const subSprite = new SpriteAsset('images/Drillship_01.png', 64, 64)
 const subLightSprite = new SpriteAsset('images/Drillship_Light.png', 256, 256)
@@ -80,10 +84,31 @@ export class Sub extends Entity {
         this.dx *= DRAG_FACTOR
         this.dy *= DRAG_FACTOR
 
+        let previousX = this.x
+        let previousY = this.y
+
         this.x += this.dx * dt
         this.y += this.dy * dt
 
         this.y = Math.max(this.y, 0)
+
+        const [fillX, fillY] = tileMap.worldToFillCoords(this.x, this.y)
+        if (tileMap.getFilled(fillX, fillY)) {
+            if (this.fuel > 0) {
+                this.fuel -= DRILL_FUEL_COST
+                if (this.fuel < 0) {
+                    this.fuel = 0
+                }
+                tileMap.setFilled(fillX, fillY, false)
+
+                // TODO: Handle ore collection
+            } else {
+                this.x = previousX
+                this.y = previousY
+                this.dx = 0
+                this.dy = 0
+            }
+        }
 
         if (this.y <= 0) {
             this.oxygen += OXYGEN_REFILL_RATE * dt
@@ -91,6 +116,13 @@ export class Sub extends Entity {
                 this.oxygen = 1
             }
             targetRotation = this.facing < 0 ? Math.PI : 0
+
+            if (Math.abs(this.x) < REFUEL_MAX_DIST) {
+                this.fuel += REFUEL_RATE * dt
+                if (this.fuel > 1) {
+                    this.fuel = 1
+                }
+            }
         } else {
             this.oxygen -= OXYGEN_DRAIN_RATE * dt
             if (this.oxygen < 0) {
