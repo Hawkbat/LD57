dba951a9897ca7ecb28dea2f436acfea3a94ab3a
Joshua Thome <joshpthome@gmail.com>
2025-04-07 15:23:34 -0500

    Swap hull and drill costs

--- app/ts/shop.ts
+++ app/ts/shop.ts
@@ -31,8 +31,8 @@ interface ShopItem {
 }
 
 const SHOP_ITEMS: ShopItem[] = [
-    { name: "Increase Hull Durability", price: 30, onPurchase: () => (sub.maxHealth++, sub.health++), stock: 5, frame: 0 },
-    { name: "Improve Drill Speed (+20%)", price: 25, onPurchase: () => sub.miningSpeed += 0.2, stock: 5, frame: 1 },
+    { name: "Increase Hull Durability", price: 25, onPurchase: () => (sub.maxHealth++, sub.health++), stock: 5, frame: 0 },
+    { name: "Improve Drill Speed (+20%)", price: 30, onPurchase: () => sub.miningSpeed += 0.2, stock: 5, frame: 1 },
     { name: "Extra Oxygen Tank", price: 50, onPurchase: () => (sub.oxygenTanks++, sub.oxygen++), stock: 1, frame: 2 },
     { name: "Extra Fuel Tank", price: 40, onPurchase: () => (sub.fuelTanks++, sub.fuel++), stock: 1, frame: 3 },
     { name: "Increase Light Power", price: 35, onPurchase: () => background.lightPower += 0.25, stock: 4, frame: 6 },
